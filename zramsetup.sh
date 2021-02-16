721662b925b964633a3ffb42d47488a483bf0e22
Sky92 <39159910+suicidesky92@users.noreply.github.com>
2021-02-16 14:00:24 +0300

    Update zramsetup.sh

--- zramsetup.sh
+++ zramsetup.sh
@@ -14,7 +14,7 @@ MEMTOTAL=$(cat /proc/meminfo | head -n1 | awk '{print$2}')
 let "MEMTOTAL=MEMTOTAL/1024"
 let "SIZEDEV=MEMTOTAL/2/CPUNUMBERS"
 echo "CPUs= $CPUNUMBERS, RAM= $MEMTOTAL MB"
-read -p "I think, need $CPUNUMBERS block devices in RAM, $SIZEDEV MB each. Ok?"
+echo -e "----------------------I think, need \033[5;36m $CPUNUMBERS \033[0m block devices in RAM, \033[5;32m $SIZEDEV \033[0m MB each. ----------------------"
 let "SIZEDEV=SIZEDEV*1024*1024"
 
 
